89a20a807d04de9b4ad0afbd7a7d58fc3db8139e
Mark Vessalico <mvpyanks23@Marks-Mac.local>
2014-04-19 14:37:44 -0400

    initial commit

---
+++ Planner/Planner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Planner
+//
+//  Created by Mark Vessalico on 4/19/14.
+//  Copyright (c) 2014 Mark Vessalico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MRVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MRVAppDelegate class]));
+    }
+}
